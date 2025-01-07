2bf72098437e93c7a79307587781d4e86d5fb458
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-01-07 18:20:07 +0800

    📦 NEW: mention

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,27 +1,39 @@
+/* eslint-disable react-refresh/only-export-components */
 import "./styles.scss";
 
-import Placeholder from "@tiptap/extension-placeholder";
+import Document from "@tiptap/extension-document";
+import Mention from "@tiptap/extension-mention";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
 import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+
+import suggestion from "./suggestion.js";
 
 export default () => {
   const editor = useEditor({
     extensions: [
-      StarterKit,
-      Placeholder.configure({
-        // Use a placeholder:
-        placeholder: "Write something …",
-        // Use different placeholders depending on the node type:
-        // placeholder: ({ node }) => {
-        //   if (node.type.name === 'heading') {
-        //     return 'What’s the title?'
-        //   }
-
-        //   return 'Can you add some further context?'
-        // },
+      Document,
+      Paragraph,
+      Text,
+      Mention.configure({
+        HTMLAttributes: {
+          class: "mention",
+        },
+        suggestion,
       }),
     ],
+    content: `
+        <p>Hi everyone! Don’t forget the daily stand up at 8 AM.</p>
+        <p><span data-type="mention" data-id="Jennifer Grey"></span> Would you mind to share what you’ve been working on lately? We fear not much happened since Dirty Dancing.
+        <p><span data-type="mention" data-id="Winona Ryder"></span> <span data-type="mention" data-id="Axl Rose"></span> Let’s go through your most important points quickly.</p>
+        <p>I have a meeting with <span data-type="mention" data-id="Christina Applegate"></span> and don’t want to come late.</p>
+        <p>– Thanks, your big boss</p>
+      `,
   });
+
+  if (!editor) {
+    return null;
+  }
 
   return <EditorContent editor={editor} />;
 };

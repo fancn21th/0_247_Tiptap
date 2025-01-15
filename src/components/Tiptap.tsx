32a740e0ef1837bc73feb371ac752e221aafd985
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-01-15 14:50:24 +0800

    📦 NEW: tiptap

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -4,16 +4,31 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Markdown } from "tiptap-markdown";
 
+const markdown = `
+# Title
+## Subtitle
+`;
+
 export default () => {
   const editor = useEditor({
     content: "# Title",
     extensions: [StarterKit, Markdown],
   });
 
+  let index = 0;
+
+  const writeMarkdown = () => {
+    const t = setInterval(() => {
+      if (index < markdown.length) {
+        editor.commands.setContent(markdown.slice(0, index++));
+      } else clearInterval(t);
+    }, 10);
+  };
+
   return (
     <>
       <EditorContent editor={editor} />
-      <button onClick={() => editor.commands.setContent("# hi")}>hi</button>
+      <button onClick={writeMarkdown}>hi</button>
     </>
   );
 };
